2b2321e05d3dcc3996d6380196646cb71dfd3916
이도영(Lee Do Young) <leedoyoung.oc@gmail.com>
2024-05-06 19:14:49 +0900

    [level 0] Title: 두 수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
@@ -1,4 +1,9 @@
-function solution(num1, num2) {
-    var answer = num1 + num2;
-    return answer;
+function solution(num1, num2) {    
+    if(-50000<=num1 && num1<=50000){
+        if(-50000<=num2 && num2<=50000){
+            const answer = num1 + num2;
+
+            return answer;
+        }
+    }
 }
