6d1f040231aea85b279d08950ec18af48fc0796a
이도영(Lee Do Young) <leedoyoung.oc@gmail.com>
2024-05-08 23:01:22 +0900

    [level 0] Title: 두 수의 곱, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
@@ -1,4 +1,12 @@
+
 function solution(num1, num2) {
-    var answer = num1 * num2;
-    return answer;
+    let ans = 0;
+
+    for (let i = 1; i <= num1; i++){
+        for (let j = 1; j <= num2; j++){
+            ans = ans + 1;
+        }
+    }
+
+    return ans;
 }
